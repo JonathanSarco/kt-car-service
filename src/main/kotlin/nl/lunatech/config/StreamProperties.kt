69ec76231e365709d538672ee87a2db892bb142a
Jony Sarco <jfe.sarco@gmail.com>
2022-08-19 14:25:41 +0200

    delete build fields and fix error on stream producer creation

--- src/main/kotlin/nl/lunatech/config/StreamProperties.kt
+++ src/main/kotlin/nl/lunatech/config/StreamProperties.kt
@@ -13,9 +13,9 @@ class StreamProperties {
         fun getSerializerProperties(): Properties {
             val props = Properties()
             props[StreamsConfig.BOOTSTRAP_SERVERS_CONFIG] = "localhost:9092"
-            props[StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG] = StringSerializer::class.java
-//            props["value.serializer"] = CarSerializer::class.java
-            props[StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG] = StringSerializer::class.java
+            props["key.serializer"] = StringSerializer::class.java
+            props["value.serializer"] = StringSerializer::class.java
+//            props[StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG] = StringSerializer::class.java
             return props
         }
 
@@ -25,7 +25,6 @@ class StreamProperties {
             props[StreamsConfig.APPLICATION_ID_CONFIG] = "kar-service-kafka"
             props["group.id"] = "hello-world"
 //            props["key.deserializer"] = StringDeserializer::class.java
-
             props[StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG] = StringDeserializer::class.java
             return props
         }
