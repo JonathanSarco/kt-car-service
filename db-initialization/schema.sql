7da76ba4afeeee114932a5ee682963f164717fd1
Jony Sarco <jfe.sarco@gmail.com>
2022-08-19 15:53:48 +0200

    add car to repair, repair shop & car problem tables to schema.sql

--- db-initialization/schema.sql
+++ db-initialization/schema.sql
@@ -1,7 +1,7 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE IF NOT EXISTS car (
-  id                        uuid DEFAULT uuid_generate_v4 (),
+  id                        UUID DEFAULT uuid_generate_v4(),
   brand                     VARCHAR(100) NOT NULL,
   model                     VARCHAR(100) NOT NULL,
   engine_model              VARCHAR(100),
@@ -19,4 +19,31 @@ CREATE TABLE IF NOT EXISTS car (
   tyre                      VARCHAR(255),
   others                    VARCHAR(100),
   traction_type             VARCHAR(100)
+);
+
+CREATE TABLE IF NOT EXISTS repair_shop (
+    id                      UUID DEFAULT uuid_generate_v4(),
+    name                    VARCHAR(100) NOT NULL,
+    supported_car_brands    TEXT[],
+    opening_hour            NUMERIC,
+    closing_hour            NUMERIC,
+    creation_date           DATE,
+    CONSTRAINT repair_shop_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS car_problem (
+   id                      UUID DEFAULT uuid_generate_v4(),
+   description             TEXT NOT NULL,
+   part_broken             VARCHAR(100),
+   broken_date             DATE,
+   CONSTRAINT car_problem_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS car_to_repair_shop (
+   id                      UUID DEFAULT uuid_generate_v4(),
+   repair_shop_id          UUID,
+   car_id                  UUID,
+   car_problem_id          UUID,
+   registered_problem_date DATE,
+   CONSTRAINT car_to_repair_shop_pkey PRIMARY KEY (id)
 );
